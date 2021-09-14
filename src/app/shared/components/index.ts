24f0c2210ed7722d98d224f60df30b1a99f2f991
Leandro Matias <leandrobaldo@tokenlab.com.br>
2021-09-14 16:52:41 -0300

    Add footer component

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,5 +1,7 @@
+import { FooterComponent } from './footer/footer.component';
 import { LoadingComponent } from './loading/loading.component';
 
 export const components = [
+  FooterComponent,
   LoadingComponent,
 ];
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { components } from './components';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [

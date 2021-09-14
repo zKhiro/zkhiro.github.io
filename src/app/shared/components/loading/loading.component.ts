57fe6fdb580d6647b030814769bad9d9deea0bb6
Leandro <leandro.khiro@gmail.com>
2021-09-13 22:12:46 -0300

    Make loading fullscreen and enhance animation

--- src/app/shared/components/loading/loading.component.ts
+++ src/app/shared/components/loading/loading.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.scss']
 })
-export class LoadingComponent {
-  
+export class LoadingComponent implements OnInit, OnDestroy {
+
+  readonly overflowHiddenClassName = 'overflow-hidden';
+
+  ngOnInit(): void {
+    this.getBodyElement()?.classList.add(this.overflowHiddenClassName);
+  }
+
+  ngOnDestroy(): void {
+    this.getBodyElement()?.classList.remove(this.overflowHiddenClassName);
+  }
+
+  getBodyElement = (): HTMLBodyElement | null => document.querySelector('body');
 }

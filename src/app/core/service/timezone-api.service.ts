622ffdc34e70e62c42fbc69917b5c4e89e55cfff
Leandro <leandro.khiro@gmail.com>
2021-09-25 17:37:41 -0300

    Doc TimezoneApiService

--- src/app/core/service/timezone-api.service.ts
+++ src/app/core/service/timezone-api.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { TimezoneApiResponse } from '@models';
 
 @Injectable()
@@ -7,7 +9,8 @@ export class TimezoneApiService {
 
   constructor(private readonly httpClient: HttpClient) { }
 
-  getTimezone() {
+  /** Make a `GET` HTTP call to WorldTimeAPI to receive São Paulo current timezone. */
+  getTimezone(): Observable<TimezoneApiResponse> {
     return this.httpClient.get<TimezoneApiResponse>('http://worldtimeapi.org/api/timezone/America/Sao_Paulo');
   }
 }

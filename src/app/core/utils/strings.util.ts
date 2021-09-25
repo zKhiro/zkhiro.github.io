6af136b91fccd6c034e1434eccaa460d0a8f8463
Leandro <leandro.khiro@gmail.com>
2021-09-25 17:38:09 -0300

    Add my birthdate

--- src/app/core/utils/strings.util.ts
+++ src/app/core/utils/strings.util.ts
@@ -2,6 +2,7 @@ import { InjectionToken } from '@angular/core';
 
 export const COMMON_STRINGS = new InjectionToken<typeof CommonStrings>('common.strings');
 export const CommonStrings = {
-  name:     'Leandro Matias Baldo',
-  position: 'dev front-end;',
+  name:       'Leandro Matias Baldo',
+  position:   'dev front-end;',
+  birthdate:  '1997-02-18T00:00',
 }

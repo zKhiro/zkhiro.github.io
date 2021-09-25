16ef80451e1efe9fc8e08f4a63814c10f6f9f1c3
Leandro <leandro.khiro@gmail.com>
2021-09-25 17:39:42 -0300

    Calculate my age and give a transition to size of the progress bar

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,9 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import * as moment from 'moment';
 import { tap } from 'rxjs/operators';
 
-
-import { COMMON_STRINGS, CommonStrings } from '@utils';
+import { Component, Inject, OnInit } from '@angular/core';
 import { TimezoneApiService } from '@service/timezone-api.service';
+import { COMMON_STRINGS, CommonStrings } from '@utils';
 
 @Component({
   selector: 'app-header',
@@ -13,7 +13,14 @@ import { TimezoneApiService } from '@service/timezone-api.service';
 })
 export class HeaderComponent implements OnInit {
 
-  totalExp = 126;
+  /** Level actually means my age. */
+  currentLevel: number;
+
+  /** Experience correspond one day of a year. */
+  remainingExperienceToNextLevel: number;
+
+  /** Represent percentage of my next birthdate. */
+  experienceProgress: number;
 
 
   constructor(
@@ -22,18 +29,44 @@ export class HeaderComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.calculateAge();
+    this.getCurrentDate();
   }
 
-  private calculateAge() {
+  private getCurrentDate(): void {
     this.timezoneApiService.getTimezone().pipe(
       tap(response => {
-        const today       = new Date(response.datetime);
-        const myBirthdate = new Date('1997-02-18T00:00');
+        this.currentLevel = this.calculateMyAge(response.datetime);
 
-        this.totalExp
-      })
+        this.calculateProgressToMyNextBirthdate(response.datetime);
+      }),
     ).subscribe();
+  }
 
+  /**
+   * Calculate my current age and returns it.
+   *
+   * @param currentDate - An ISO8601-valid string representing the current local date/time.
+   */
+  private calculateMyAge(currentDate: string): number {
+    const today       = moment(currentDate);
+    const myBirthdate = moment(this.commonStrings.birthdate);
+
+    return today.diff(myBirthdate, 'years');
+  }
+
+  /**
+   * Calculate how many days remain to my next birthdate and the porcentage to it.
+   *
+   * This should be invoked after `calculateMyAge()` finished.
+   *
+   * @param currentDate - An ISO8601-valid string representing the current local date/time.
+   */
+  private calculateProgressToMyNextBirthdate(currentDate: string): void {
+    const today             = moment(currentDate);
+    const thisYearBirthdate = moment(this.commonStrings.birthdate).add(this.currentLevel, 'year');
+    const nextYearBirthdate = thisYearBirthdate.clone().add(1, 'year');
+
+    this.remainingExperienceToNextLevel = nextYearBirthdate.diff(today, 'days');
+    this.experienceProgress             = (nextYearBirthdate.diff(thisYearBirthdate, 'days') - this.remainingExperienceToNextLevel) / nextYearBirthdate.diff(thisYearBirthdate, 'days') * 100;
   }
 }

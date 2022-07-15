f030f772aca0b91b96fbb285d042ebd560aac778
Leandro Matias <leandrobaldo@tokenlab.com.br>
2022-07-15 16:57:38 -0300

    Refactor home route and add content to home

---
+++ src/app/views/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  host: { 'class': 'd-flex my-auto w-100' }
+})
+export class HomeComponent {}

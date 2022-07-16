ec7b9e8a68fbabc46e6e4aab90c3a6ab240dcda3
Leandro <leandro.khiro@gmail.com>
2022-07-16 15:19:46 -0300

    Fix responsiveness

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  host: { 'class': 'd-flex my-auto w-100' }
+  host: { 'class': 'd-flex flex-column flex-lg-row align-self-center align-items-center gap-4 gap-lg-6 w-100' }
 })
 export class HomeComponent {}

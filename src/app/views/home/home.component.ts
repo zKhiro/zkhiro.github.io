dce5ffb997244dcf5c21f8a8ddc176a494d3a5e1
Leandro <leandro.khiro@gmail.com>
2022-07-17 12:59:06 -0300

    Now container css class is handle inside each component and group some home.component styles in home-container class

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  host: { 'class': 'd-flex flex-column flex-lg-row align-self-center align-items-center gap-4 gap-lg-6 w-100' }
+  host: { 'class': 'home-container container flex-column flex-lg-row gap-4 gap-lg-6' }
 })
 export class HomeComponent {}

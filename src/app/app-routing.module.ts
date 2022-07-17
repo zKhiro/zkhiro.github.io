9fae3f72934122620ced33294987c30e5bfac02e
Leandro <leandro.khiro@gmail.com>
2022-07-17 11:15:53 -0300

    Apply navbar resolver to existing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarResolver } from '@resolvers';
 
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule),
+    resolve: {
+      navbar: NavbarResolver,
+    },
+    data: {
+      showNavbar: false,
+    }
   },
   {
     path: 'skill-tree',
     loadChildren: () => import('./views/skill-tree/skill-tree.module').then(m => m.SkillTreeModule),
+    resolve: {
+      navbar: NavbarResolver,
+    },
+    data: {
+      showNavbar: true,
+      title: 'Skill Tree'
+    }
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }

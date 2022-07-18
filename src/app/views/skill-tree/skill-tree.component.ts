be904cae5a6e0cacf62ccfaab0fa33a1a5dee295
Leandro Matias <leandrobaldo@tokenlab.com.br>
2022-07-18 16:01:20 -0300

    Include svg skill tree component and center element

--- src/app/views/skill-tree/skill-tree.component.ts
+++ src/app/views/skill-tree/skill-tree.component.ts
@@ -4,5 +4,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-skill-tree',
   templateUrl: './skill-tree.component.html',
+  host: { class: 'container-fluid d-flex justify-content-center' }
 })
 export class SkillTreeComponent {}

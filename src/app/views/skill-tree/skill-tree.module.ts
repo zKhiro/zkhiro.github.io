915552fb10cd722bd0e85d5b80f6bd9e9ab8f31e
Leandro <leandro.khiro@gmail.com>
2022-07-26 13:37:07 -0300

    Use new skill popover component

--- src/app/views/skill-tree/skill-tree.module.ts
+++ src/app/views/skill-tree/skill-tree.module.ts
@@ -1,6 +1,11 @@
+
+
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SkillHandlerModule } from '@directives';
+import { SkillService } from '@services';
 
+import { SkillPopoverComponent } from './skill-popover/skill-popover.component';
 import { SkillTreeRoutingModule } from './skill-tree-routing.module';
 import { SkillTreeComponent } from './skill-tree.component';
 import { SvgSkillTreeComponent } from './svg-skill-tree/svg-skill-tree.component';
@@ -10,10 +15,16 @@ import { SvgSkillTreeComponent } from './svg-skill-tree/svg-skill-tree.component
   declarations: [
     SkillTreeComponent,
     SvgSkillTreeComponent,
+    SkillPopoverComponent,
   ],
   imports: [
     CommonModule,
     SkillTreeRoutingModule,
+
+    SkillHandlerModule,
+  ],
+  providers: [
+    SkillService,
   ]
 })
 export class SkillTreeModule { }

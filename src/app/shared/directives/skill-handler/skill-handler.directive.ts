ba3604f5f6ee20783cf34d51fd9b3004a19d6932
Leandro <leandro.khiro@gmail.com>
2022-07-26 13:24:07 -0300

    Add possibles interactions

--- src/app/shared/directives/skill-handler/skill-handler.directive.ts
+++ src/app/shared/directives/skill-handler/skill-handler.directive.ts
@@ -1,9 +1,51 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
+import { SkillService } from '@services';
+
 
 @Directive({
-  selector: '[appSkillHandler]'
+  selector: '[skillHandler]'
 })
 export class SkillHandlerDirective {
 
-  constructor(private element: ElementRef<HTMLElement>) { }
+  constructor(
+    private element: ElementRef<HTMLElement>,
+    private skillService: SkillService,
+  ) { }
+
+  @HostListener('mouseenter')
+  private mouseEnterEvent(event: MouseEvent) {
+    if (!this.skillService.active) {
+      this.setSkill();
+    }
+  }
+
+  @HostListener('mouseleave')
+  private mouseLeaveEvent(event: MouseEvent) {
+    this.skillService.active = this.skillService.hasFocus;
+  }
+
+  @HostListener('focus')
+  private focusEvent() {
+    this.skillService.hasFocus = true;
+
+    this.setSkill();
+  }
+
+  @HostListener('blur')
+  private blurEvent() {
+    this.skillService.reset();
+  }
+
+
+  private getParentElement    = () => (this.element.nativeElement.parentElement as HTMLElement);
+  private getParentElementId  = () => this.getParentElement().id;
+
+  private setSkill() {
+    this.skillService.active  = true;
+    this.skillService.title   = this.getParentElementId();
+
+    this.skillService.popoverPosition = this.getParentElement().getBoundingClientRect();
+    this.skillService.popoverClass    = `skill-${this.getParentElement().dataset.type}`;
+  }
+
 }
